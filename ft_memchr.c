7e5d6a288d43c59052542f94b574253594c71a30
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-01 20:12:33 +0300

    Refactor to respect the norm.

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,12 +6,11 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 13:19:15 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/01 17:19:04 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/01 18:43:09 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 /*
 ** Searches within the first num bytes of the block of memory pointed by
@@ -19,14 +18,31 @@
 ** and returns a pointer to it.
 */
 
-static inline void		init_blocks(uint64_t *repeated_one, uint64_t *repeated_c,
-							uint8_t *chr)
+static inline void		init_blocks(uint64_t *repeated_one, \
+							uint64_t *repeated_c, uint8_t *chr)
 {
 	*repeated_one = 0x0101010101010101;
 	*repeated_c = *chr;
 	*repeated_c |= *repeated_c << 8;
 	*repeated_c |= *repeated_c << 16;
 	*repeated_c |= *repeated_c << 32;
+}
+
+static inline void		test_longword(const uint64_t **longword_ptr, \
+							size_t *num, uint64_t repeated_c, \
+							uint64_t repeated_one)
+{
+	uint64_t	longword1;
+
+	while (*num >= 8)
+	{
+		longword1 = **longword_ptr ^ repeated_c;
+		if ((((longword1 - repeated_one) & ~longword1) \
+			& (repeated_one << 7)) != 0)
+			break ;
+		++*longword_ptr;
+		*num -= 8;
+	}
 }
 
 void					*ft_memchr(const void *ptr, int c, size_t num)
@@ -48,22 +64,10 @@ void					*ft_memchr(const void *ptr, int c, size_t num)
 	}
 	longword_ptr = (const uint64_t *)chr_ptr;
 	init_blocks(&repeated_one, &repeated_c, &chr);
-	while (num >= 8)
-	{
-		uint64_t	longword1 = *longword_ptr ^ repeated_c;
-		if ((((longword1 - repeated_one) & ~longword1) \
-			& (repeated_one << 7)) != 0)
-			break;
-		longword_ptr++;
-		num -= 8;
-	}
+	test_longword(&longword_ptr, &num, repeated_c, repeated_one);
 	chr_ptr = (const uint8_t *)longword_ptr;
-	while (num > 0)
-	{
-		if (*chr_ptr == chr)
-			return ((void *)chr_ptr);
-		++chr_ptr;
-		--num;
-	}
+	while (num-- > 0)
+		if (*chr_ptr++ == chr)
+			return ((void *)chr_ptr - 1);
 	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 08:04:18 by amalliar          #+#    #+#             */
-/*   Updated: 2020/04/30 18:56:33 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/01 20:12:21 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 # include <stddef.h>
 # include <stdint.h>
 
-void	*ft_memset(void *ptr, int val, size_t num);
 void	ft_bzero(void *ptr, size_t num);
+void	*ft_memset(void *ptr, int c, size_t num);
 void	*ft_memcpy(void *dest, const void *src, size_t num);
+void	*ft_memchr(const void *ptr, int c, size_t num);
 
 #endif
