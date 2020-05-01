b5d8ceda5d63eb97970dc66779be7b010e295a7c
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-01 20:11:18 +0300

    Fix norm errors.

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 14:00:28 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/01 13:09:09 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/01 20:06:28 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,24 @@
 ** ptr to the specified value c (interpreted as an unsigned char).
 */
 
-static inline void		write_block1B(uint64_t *dest, uint64_t *block, size_t *num)
+static inline void		write_block8b(uint64_t *dest, uint64_t *block, \
+							size_t *num)
 {
 	((uint8_t *)*dest)[0] = *block;
 	*dest += 1;
 	*num -= 1;
 }
 
-static inline void		write_block8B(uint64_t *dest, uint64_t *block, size_t *num)
+static inline void		write_block64b(uint64_t *dest, uint64_t *block, \
+							size_t *num)
 {
 	((uint64_t *)*dest)[0] = *block;
 	*dest += 8;
 	*num -= 8;
 }
 
-static inline void		write_block64B(uint64_t *dest, uint64_t *block, size_t *num)
+static inline void		write_block512b(uint64_t *dest, uint64_t *block, \
+							size_t *num)
 {
 	((uint64_t *)*dest)[0] = *block;
 	((uint64_t *)*dest)[1] = *block;
@@ -58,13 +61,13 @@ void					*ft_memset(void *ptr, int c, size_t num)
 		block |= block << 16;
 		block |= block << 32;
 		while (dest % 8)
-			write_block1B(&dest, &block, &num);
+			write_block8b(&dest, &block, &num);
 		while (num >= 64)
-			write_block64B(&dest, &block, &num);
+			write_block512b(&dest, &block, &num);
 		while (num >= 8)
-			write_block8B(&dest, &block, &num);
+			write_block64b(&dest, &block, &num);
 	}
 	while (num)
-		write_block1B(&dest, &block, &num);
+		write_block8b(&dest, &block, &num);
 	return (ptr);
 }
