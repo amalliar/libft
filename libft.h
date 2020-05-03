c28126eef64803678cd178e3ba8a667680ba4523
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-03 21:28:04 +0300

    Add ft_isdigit().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 08:04:18 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/03 20:06:46 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/03 21:27:05 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void		*ft_memchr(const void *ptr, int c, size_t num);
 void		*ft_calloc(size_t num, size_t size);
 int			ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
 int			ft_isalpha(int c);
+int			ft_isdigit(int c);
 size_t		ft_strlen(const char *str);
 size_t		ft_strlcpy(char *dest, const char *src, size_t num);
 char		*ft_strdup(const char *str);
