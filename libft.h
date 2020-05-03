601baf3ca98a0a17ec35ead9115a187487772fe8
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-03 21:32:21 +0300

    Add ft_isalnum().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 08:04:18 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/03 21:27:05 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/03 21:29:33 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void		*ft_calloc(size_t num, size_t size);
 int			ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
+int			ft_isalnum(int c);
 size_t		ft_strlen(const char *str);
 size_t		ft_strlcpy(char *dest, const char *src, size_t num);
 char		*ft_strdup(const char *str);
