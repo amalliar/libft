0d9039e6c3d9409ebd35111c3d496dbee760a59b
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-03 21:53:46 +0300

    Add ft_tolower().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 08:04:18 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/03 21:45:25 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/03 21:51:27 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int			ft_isalnum(int c);
 int			ft_isascii(int c);
 int			ft_isprint(int c);
 int			ft_toupper(int c);
+int			ft_tolower(int c);
 size_t		ft_strlen(const char *str);
 size_t		ft_strlcpy(char *dest, const char *src, size_t num);
 char		*ft_strdup(const char *str);
