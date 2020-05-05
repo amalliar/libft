e2fe8865d35cda51c12ba1c97af6fbdb56dbb524
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-05 16:54:20 +0300

    Change magic numbers' aliases to match other functions.

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 13:19:15 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/03 15:13:32 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/05 16:53:22 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,27 @@
 ** and returns a pointer to it.
 */
 
-static inline void		init_blocks(uint64_t *repeated_one, \
-							uint64_t *repeated_c, uint8_t *chr)
+static inline void		init_magic(uint64_t *lomagic, \
+							uint64_t *chmagic, uint8_t *chr)
 {
-	*repeated_one = 0x0101010101010101;
-	*repeated_c = *chr;
-	*repeated_c |= *repeated_c << 8;
-	*repeated_c |= *repeated_c << 16;
-	*repeated_c |= *repeated_c << 32;
+	*lomagic = 0x0101010101010101;
+	*chmagic = *chr;
+	*chmagic |= *chmagic << 8;
+	*chmagic |= *chmagic << 16;
+	*chmagic |= *chmagic << 32;
 }
 
 static inline void		test_longword(const uint64_t **longword_ptr, \
-							size_t *num, uint64_t repeated_c, \
-							uint64_t repeated_one)
+							size_t *num, uint64_t chmagic, uint64_t lomagic)
 {
-	uint64_t	longword1;
+	uint64_t	longword;
+	uint64_t	himagic;
 
+	himagic = 0x8080808080808080;
 	while (*num >= 8)
 	{
-		longword1 = **longword_ptr ^ repeated_c;
-		if ((((longword1 - repeated_one) & ~longword1) \
-			& (repeated_one << 7)) != 0)
+		longword = **longword_ptr ^ chmagic;
+		if (((longword - lomagic) & ~longword & himagic) != 0)
 			break ;
 		++*longword_ptr;
 		*num -= 8;
@@ -49,8 +49,8 @@ void					*ft_memchr(const void *ptr, int c, size_t num)
 {
 	const uint8_t	*chr_ptr;
 	const uint64_t	*longword_ptr;
-	uint64_t		repeated_one;
-	uint64_t		repeated_c;
+	uint64_t		lomagic;
+	uint64_t		chmagic;
 	uint8_t			chr;
 
 	chr = (uint8_t)c;
@@ -63,8 +63,8 @@ void					*ft_memchr(const void *ptr, int c, size_t num)
 		--num;
 	}
 	longword_ptr = (const uint64_t *)chr_ptr;
-	init_blocks(&repeated_one, &repeated_c, &chr);
-	test_longword(&longword_ptr, &num, repeated_c, repeated_one);
+	init_magic(&lomagic, &chmagic, &chr);
+	test_longword(&longword_ptr, &num, chmagic, lomagic);
 	chr_ptr = (const uint8_t *)longword_ptr;
 	while (num-- != 0)
 		if (*chr_ptr++ == chr)
