446b8dc1369feb2091cecdd47f14af58402ec44d
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-06 17:57:27 +0300

    Add ft_strncmp().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 08:04:18 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/05 17:27:35 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/05 17:34:51 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ int			ft_isprint(int c);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
 int			ft_atoi(const char *str);
+int			ft_strncmp(const char *str1, const char *str2, size_t num);
 size_t		ft_strlen(const char *str);
 size_t		ft_strlcpy(char *dest, const char *src, size_t num);
 size_t		ft_strlcat(char *dest, const char *src, size_t num);
