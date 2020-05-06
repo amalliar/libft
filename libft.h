6ca646a72291f36ab15db0b2c45ab674d6fe1d0a
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-06 22:54:40 +0300

    Add ft_strmapi().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 08:04:18 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/06 19:35:33 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/06 22:31:36 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ char		*ft_itoa(int num);
 char		*ft_strchr(char *str, int c);
 char		*ft_strrchr(const char *str, int c);
 char		*ft_strnstr(const char *big, const char *little, size_t num);
+char		*ft_strmapi(const char *str, char (*func)(unsigned int, char));
 
 #endif
