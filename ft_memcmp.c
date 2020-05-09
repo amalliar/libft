d0902148d449c7007c4352a235b87b599128bf81
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-09 23:15:15 +0300

    Refactor for better readability.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,11 +6,13 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/03 13:55:47 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/03 15:11:54 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/09 23:07:23 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+typedef unsigned long	t_longword;
 
 /*
 ** Compares the first num bytes of the block of memory pointed by ptr1 to the
@@ -18,43 +20,40 @@
 ** different from zero representing which is greater if they do not.
 */
 
-static inline int	byte_cmp(uint64_t *pt1, uint64_t *pt2, \
-						size_t *num)
+static inline int		byte_cmp(t_longword *pt1, t_longword *pt2, \
+							size_t *num)
 {
-	*pt1 += 1;
-	*pt2 += 1;
-	*num -= 1;
-	return (((uint8_t *)*pt1)[-1] - ((uint8_t *)*pt2)[-1]);
+	++*pt1;
+	++*pt2;
+	--*num;
+	return (((unsigned char *)*pt1)[-1] - ((unsigned char *)*pt2)[-1]);
 }
 
-static inline int	word_cmp(uint64_t *pt1, uint64_t *pt2, \
-						size_t *num)
+int						ft_memcmp(const void *ptr1, const void *ptr2, \
+							size_t num)
 {
-	*pt1 += 8;
-	*pt2 += 8;
-	*num -= 8;
-	return (((uint64_t *)*pt1)[-1] - ((uint64_t *)*pt2)[-1]);
-}
+	t_longword		pt1;
+	t_longword		pt2;
+	int				res;
 
-int					ft_memcmp(const void *ptr1, const void *ptr2, size_t num)
-{
-	uint64_t	pt1;
-	uint64_t	pt2;
-	int			res;
-
-	pt1 = (uint64_t)ptr1;
-	pt2 = (uint64_t)ptr2;
-	if (num >= 8)
+	pt1 = (t_longword)ptr1;
+	pt2 = (t_longword)ptr2;
+	if (num >= sizeof(t_longword))
 	{
-		while (pt1 % 8 != 0)
-			if ((res = byte_cmp(&pt1, &pt2, &num)))
+		while (pt1 % sizeof(t_longword) != 0)
+			if ((res = byte_cmp(&pt1, &pt2, &num)) != 0)
 				return (res);
-		while (num >= 8)
-			if ((res = word_cmp(&pt1, &pt2, &num)))
-				return (res);
+		while (num >= sizeof(t_longword))
+		{
+			if (((t_longword *)pt1)[0] != ((t_longword *)pt2)[0])
+				break ;
+			pt1 += sizeof(t_longword);
+			pt2 += sizeof(t_longword);
+			num -= sizeof(t_longword);
+		}
 	}
 	while (num != 0)
-		if ((res = byte_cmp(&pt1, &pt2, &num)))
+		if ((res = byte_cmp(&pt1, &pt2, &num)) != 0)
 			return (res);
 	return (0);
 }
