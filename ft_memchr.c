9de6712329b36dc3042678b1333e53282abf72c0
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-09 22:23:57 +0300

    Make this function work for 32-bit longwords.

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,11 +6,13 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 13:19:15 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/05 16:53:22 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/09 22:20:01 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+typedef unsigned long	t_longword;
 
 /*
 ** Searches within the first num bytes of the block of memory pointed by
@@ -18,56 +20,75 @@
 ** and returns a pointer to it.
 */
 
-static inline void		init_magic(uint64_t *lomagic, \
-							uint64_t *chmagic, uint8_t *chr)
+static inline void		init_magic(t_longword *lomagic, \
+							t_longword *chmagic, int c)
 {
-	*lomagic = 0x0101010101010101;
-	*chmagic = *chr;
+	*lomagic = 0x01010101;
+	*chmagic = c;
 	*chmagic |= *chmagic << 8;
 	*chmagic |= *chmagic << 16;
-	*chmagic |= *chmagic << 32;
+	if (sizeof(t_longword) > 4)
+	{
+		*lomagic |= (*lomagic << 16) << 16;
+		*chmagic |= (*chmagic << 16) << 16;
+	}
 }
 
-static inline void		test_longword(const uint64_t **longword_ptr, \
-							size_t *num, uint64_t chmagic, uint64_t lomagic)
-{
-	uint64_t	longword;
-	uint64_t	himagic;
+/*
+**           byte1     byte2          byte8(4)
+**
+** lomagic:  00000001  00000001  ...  00000001
+**
+** himagic:  10000000  10000000  ...  10000000
+**
+** chmagic:  01000001  01000001  ...  01000001
+** (for c == 'A')
+*/
 
-	himagic = 0x8080808080808080;
-	while (*num >= 8)
+static inline void		test_longwords(const t_longword **longword_ptr, \
+							size_t *num, t_longword lomagic, t_longword chmagic)
+{
+	t_longword		longword;
+	t_longword		himagic;
+
+	himagic = 0x80808080;
+	if (sizeof(t_longword) > 4)
+		himagic |= (himagic << 16) << 16;
+	while (*num >= sizeof(t_longword))
 	{
 		longword = **longword_ptr ^ chmagic;
 		if (((longword - lomagic) & ~longword & himagic) != 0)
 			break ;
 		++*longword_ptr;
-		*num -= 8;
+		*num -= sizeof(t_longword);
 	}
 }
 
 void					*ft_memchr(const void *ptr, int c, size_t num)
 {
-	const uint8_t	*chr_ptr;
-	const uint64_t	*longword_ptr;
-	uint64_t		lomagic;
-	uint64_t		chmagic;
-	uint8_t			chr;
+	const unsigned char		*char_ptr;
+	const t_longword		*longword_ptr;
+	t_longword				lomagic;
+	t_longword				chmagic;
 
-	chr = (uint8_t)c;
-	chr_ptr = (const uint8_t *)ptr;
-	while (num != 0 && (size_t)chr_ptr % 8 != 0)
+	c = (unsigned char)c;
+	char_ptr = (const unsigned char *)ptr;
+	while (num != 0 && (size_t)char_ptr % sizeof(t_longword) != 0)
 	{
-		if (*chr_ptr == chr)
-			return ((void *)chr_ptr);
-		++chr_ptr;
+		if (*char_ptr == c)
+			return ((void *)char_ptr);
+		++char_ptr;
 		--num;
 	}
-	longword_ptr = (const uint64_t *)chr_ptr;
-	init_magic(&lomagic, &chmagic, &chr);
-	test_longword(&longword_ptr, &num, chmagic, lomagic);
-	chr_ptr = (const uint8_t *)longword_ptr;
+	longword_ptr = (const t_longword *)char_ptr;
+	init_magic(&lomagic, &chmagic, c);
+	test_longwords(&longword_ptr, &num, lomagic, chmagic);
+	char_ptr = (const unsigned char *)longword_ptr;
 	while (num-- != 0)
-		if (*chr_ptr++ == chr)
-			return ((void *)chr_ptr - 1);
+	{
+		if (*char_ptr == c)
+			return ((void *)char_ptr);
+		++char_ptr;
+	}
 	return (NULL);
 }
