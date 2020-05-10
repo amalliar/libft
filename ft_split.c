d0ba79260d52dd7281a9a11c5e563bdcbd02785a
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-10 17:59:51 +0300

    Clear 'word_tab' if malloc() fails.

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 23:06:02 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/09 19:34:47 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/10 17:57:43 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,18 @@ static void		*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 static int		null_terminate(const char *str, char ***word_tab, \
 					size_t *tab_size)
 {
-	const char		*ptr;
+	size_t		len;
 
-	if ((ptr = str + ft_strlen(str)) != str)
+	len = ft_strlen(str);
+	if (len != 0)
 	{
 		if (!(*word_tab = (char **)ft_realloc(*word_tab, *tab_size * \
 			sizeof(char *), (*tab_size + 1) * sizeof(char *))))
 			return (1);
-		if (!((*word_tab)[*tab_size] = (char *)malloc(ptr - str + 1)))
+		if (!((*word_tab)[*tab_size] = (char *)malloc(len + 1)))
 			return (1);
-		ft_memcpy((*word_tab)[*tab_size], str, ptr - str);
-		(*word_tab)[*tab_size][ptr - str] = '\0';
+		ft_memcpy((*word_tab)[*tab_size], str, len);
+		(*word_tab)[*tab_size][len] = '\0';
 		++*tab_size;
 	}
 	if (!(*word_tab = (char **)ft_realloc(*word_tab, *tab_size * \
@@ -62,13 +63,21 @@ static int		null_terminate(const char *str, char ***word_tab, \
 	return (0);
 }
 
+static void		*clear_tab(char **word_tab, size_t tab_size)
+{
+	while (tab_size-- != 0)
+		if (word_tab[tab_size] != NULL)
+			free(word_tab[tab_size]);
+	return (NULL);
+}
+
 char			**ft_split(const char *str, char c)
 {
 	const char		*ptr;
 	char			**word_tab;
 	size_t			tab_size;
 
-	if (!str || (word_tab = NULL))
+	if (str == NULL || (word_tab = NULL))
 		return (NULL);
 	tab_size = 0;
 	while ((ptr = ft_strchr(str, c)) != NULL)
@@ -77,9 +86,9 @@ char			**ft_split(const char *str, char c)
 		{
 			if (!(word_tab = (char **)ft_realloc(word_tab, tab_size * \
 				sizeof(char *), (tab_size + 1) * sizeof(char *))))
-				return (NULL);
+				return (clear_tab(word_tab, tab_size));
 			if (!(word_tab[tab_size] = (char *)malloc(ptr - str + 1)))
-				return (NULL);
+				return (clear_tab(word_tab, tab_size));
 			ft_memcpy(word_tab[tab_size], str, ptr - str);
 			word_tab[tab_size][ptr - str] = '\0';
 			++tab_size;
@@ -87,6 +96,6 @@ char			**ft_split(const char *str, char c)
 		str = ptr + 1;
 	}
 	if (null_terminate(str, &word_tab, &tab_size))
-		return (NULL);
+		return (clear_tab(word_tab, tab_size));
 	return (word_tab);
 }
