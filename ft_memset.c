9e99bdea71eb7f40dc3ac75b56e08ef59ae28d50
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-10 17:22:29 +0300

    Refactor ft_memset() for better readability.

--- ft_memset.c
+++ ft_memset.c
@@ -6,65 +6,67 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 14:00:28 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/08 23:54:40 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/10 17:21:28 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+typedef unsigned long	t_longword;
 
 /*
 ** Sets the first num bytes of the block of memory pointed by
 ** ptr to the specified value c (interpreted as an unsigned char).
 */
 
-static inline void		byte_copy_fwd(uint64_t *dest, uint64_t block, \
+static inline void		byte_copy_fwd(t_longword *dest, t_longword block, \
 							size_t *num)
 {
-	((uint8_t *)*dest)[0] = block;
-	*dest += 1;
-	*num -= 1;
+	((unsigned char *)*dest)[0] = block;
+	++*dest;
+	--*num;
 }
 
-static inline void		word_copy_fwd(uint64_t *dest, uint64_t block, \
+static inline void		word_copy_fwd(t_longword *dest, t_longword block, \
 							size_t *num)
 {
-	((uint64_t *)*dest)[0] = block;
-	*dest += 8;
-	*num -= 8;
+	((t_longword *)*dest)[0] = block;
+	*dest += sizeof(t_longword);
+	*num -= sizeof(t_longword);
 }
 
-static inline void		page_copy_fwd(uint64_t *dest, uint64_t block, \
+static inline void		page_copy_fwd(t_longword *dest, t_longword block, \
 							size_t *num)
 {
-	((uint64_t *)*dest)[0] = block;
-	((uint64_t *)*dest)[1] = block;
-	((uint64_t *)*dest)[2] = block;
-	((uint64_t *)*dest)[3] = block;
-	((uint64_t *)*dest)[4] = block;
-	((uint64_t *)*dest)[5] = block;
-	((uint64_t *)*dest)[6] = block;
-	((uint64_t *)*dest)[7] = block;
-	*dest += 64;
-	*num -= 64;
+	((t_longword *)*dest)[0] = block;
+	((t_longword *)*dest)[1] = block;
+	((t_longword *)*dest)[2] = block;
+	((t_longword *)*dest)[3] = block;
+	((t_longword *)*dest)[4] = block;
+	((t_longword *)*dest)[5] = block;
+	((t_longword *)*dest)[6] = block;
+	((t_longword *)*dest)[7] = block;
+	*dest += 8 * sizeof(t_longword);
+	*num -= 8 * sizeof(t_longword);
 }
 
 void					*ft_memset(void *ptr, int c, size_t num)
 {
-	uint64_t	dest;
-	uint64_t	block;
+	t_longword		dest;
+	t_longword		block;
 
-	dest = (uint64_t)ptr;
-	block = (uint8_t)c;
+	dest = (t_longword)ptr;
+	block = (unsigned char)c;
 	block |= block << 8;
 	block |= block << 16;
-	block |= block << 32;
-	if (num >= 8)
+	block |= (block << 16) << 16;
+	if (num >= sizeof(t_longword))
 	{
-		while (dest % 8 != 0)
+		while (dest % sizeof(t_longword) != 0)
 			byte_copy_fwd(&dest, block, &num);
-		while (num >= 64)
+		while (num >= 8 * sizeof(t_longword))
 			page_copy_fwd(&dest, block, &num);
-		while (num >= 8)
+		while (num >= sizeof(t_longword))
 			word_copy_fwd(&dest, block, &num);
 	}
 	while (num != 0)
