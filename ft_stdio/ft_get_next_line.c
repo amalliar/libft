983be7afda3a2697987ed7797b9dd8b8f5dd8510
Alexander Chetverkin <virtual-ins@mail.ru>
2020-05-28 23:42:53 +0300

    Rename fd_tab to cache to better represent its usage.

--- ft_stdio/ft_get_next_line.c
+++ ft_stdio/ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/25 10:01:30 by amalliar          #+#    #+#             */
-/*   Updated: 2020/05/25 10:09:48 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/05/28 23:41:50 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,43 +20,43 @@
 ** without the newline.
 */
 
-static int		truncate_tab_line(char **fd_tab, int fd, char *nl_ptr)
+static int		truncate_cache_line(char **cache, int fd, char *nl_ptr)
 {
 	size_t	len;
 	char	*new;
 
 	if (nl_ptr == NULL || *(nl_ptr + 1) == '\0')
 	{
-		free(fd_tab[fd]);
-		fd_tab[fd] = NULL;
+		free(cache[fd]);
+		cache[fd] = NULL;
 		return (0);
 	}
 	len = ft_strlen(nl_ptr + 1);
 	if (!(new = (char *)malloc(len + 1)))
 		return (-1);
 	ft_memcpy(new, nl_ptr + 1, len + 1);
-	free(fd_tab[fd]);
-	fd_tab[fd] = new;
+	free(cache[fd]);
+	cache[fd] = new;
 	return (0);
 }
 
-static int		get_tab_line(char **fd_tab, int fd, char **line)
+static int		get_cache_line(char **cache, int fd, char **line)
 {
 	char	*nl_ptr;
 	size_t	len;
 
-	if (fd_tab[fd] != NULL)
+	if (cache[fd] != NULL)
 	{
-		nl_ptr = ft_strchr(fd_tab[fd], '\n');
+		nl_ptr = ft_strchr(cache[fd], '\n');
 		if (nl_ptr == NULL)
-			len = ft_strlen(fd_tab[fd]);
+			len = ft_strlen(cache[fd]);
 		else
-			len = nl_ptr - fd_tab[fd];
+			len = nl_ptr - cache[fd];
 		if (!(*line = (char *)malloc(len + 1)))
 			return (-1);
 		(*line)[len] = '\0';
-		ft_memcpy(*line, fd_tab[fd], len);
-		if (truncate_tab_line(fd_tab, fd, nl_ptr))
+		ft_memcpy(*line, cache[fd], len);
+		if (truncate_cache_line(cache, fd, nl_ptr))
 			return (-1);
 		return ((nl_ptr == NULL) ? 0 : 1);
 	}
@@ -66,7 +66,7 @@ static int		get_tab_line(char **fd_tab, int fd, char **line)
 	return (0);
 }
 
-static int		process_buffer(char **fd_tab, int fd, char **line, char *buffer)
+static int		process_buffer(char **cache, int fd, char **line, char *buffer)
 {
 	char	*nl_ptr;
 	char	*new;
@@ -86,22 +86,22 @@ static int		process_buffer(char **fd_tab, int fd, char **line, char *buffer)
 	free(*line);
 	*line = new;
 	if (*(nl_ptr + 1) != '\0')
-		if (!(fd_tab[fd] = ft_strdup(nl_ptr + 1)))
+		if (!(cache[fd] = ft_strdup(nl_ptr + 1)))
 			return (-1);
 	return (1);
 }
 
-static void		free_buffers(char **fd_tab, char **line)
+static void		free_buffers(char **cache, char **line)
 {
 	int		i;
 
 	i = 0;
 	while (i < 1024)
 	{
-		if (fd_tab[i] != NULL)
+		if (cache[i] != NULL)
 		{
-			free(fd_tab[i]);
-			fd_tab[i] = NULL;
+			free(cache[i]);
+			cache[i] = NULL;
 		}
 		++i;
 	}
@@ -114,27 +114,27 @@ static void		free_buffers(char **fd_tab, char **line)
 
 int				ft_get_next_line(int fd, char **line)
 {
-	static char		*fd_tab[1024] = {NULL};
+	static char		*cache[1024] = {NULL};
 	char			buffer[BUFFER_SIZE + 1];
 	int				ret;
 
 	if (fd < 0 || fd > 1023 || line == NULL)
 		return (-1);
-	ret = get_tab_line(fd_tab, fd, line);
+	ret = get_cache_line(cache, fd, line);
 	if (ret != 0)
 	{
 		if (ret == -1)
-			free_buffers(fd_tab, line);
+			free_buffers(cache, line);
 		return (ret);
 	}
 	while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[ret] = '\0';
-		ret = process_buffer(fd_tab, fd, line, buffer);
+		ret = process_buffer(cache, fd, line, buffer);
 		if (ret != 0)
 			break ;
 	}
 	if (ret == -1)
-		free_buffers(fd_tab, line);
+		free_buffers(cache, line);
 	return (ret);
 }
